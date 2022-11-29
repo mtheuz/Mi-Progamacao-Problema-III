ddd18b210df5ac1154e7165243d790c785dea3a5
mailsonalves <mailsonalves.new@gmail.com>
2022-11-28 21:31:44 -0300

    new

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ package application;
 import java.io.IOException;
 import java.net.URL;
 
+import application.controller.TelaInicio;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -22,7 +23,8 @@ public class Main extends Application {
 		FXMLLoader loader = new FXMLLoader();
 		URL xmlURL = getClass().getResource("/application/view/TelaInicio.fxml");
 		loader.setLocation(xmlURL);
-		
+		loader.setController(new TelaInicio());
+		loader.getController();
 		Parent parent = loader.load();
 		Scene scene = new Scene(parent);
 		primaryStage.setScene(scene);
